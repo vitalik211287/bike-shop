0bbbbe142b65f7d988ed3ed1c0fabbbd2d707e6f
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-04-16 16:59:46 +0300

    Create BurgerMenu.jsx

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,8 @@
+import { useUser } from 'UserContext';
+import Navigation from 'components/Nav/Navigation';
+
+const BurgerMenu = () => {
+  const { isActive } = useUser();
+  return <div>{isActive && <Navigation />}</div>;
+};
+export default BurgerMenu;
