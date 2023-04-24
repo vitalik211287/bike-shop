866781e89a8fac88100122eb67424574765da601
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2023-04-24 20:09:59 +0300

    recomended

--- src/components/Home/Recomended/Recomended.jsx
+++ src/components/Home/Recomended/Recomended.jsx
@@ -1,41 +1,51 @@
 // import { NavLink } from 'react-router-dom';
 import IconButton from 'components/button';
-import { Text } from '../Main/Main.styled';
-
+import { Section, Text } from '../Main/Main.styled';
+import collection from '../../../../src/velo.json';
 import {
   CartIcone,
   HeartIcone,
 } from 'components/ButtonBarHeader/ButtonBarHeader.styled';
-import { Div, Section } from './Recomended.styled';
+import { Box, P } from './Recomended.styled';
+import {
+  ColorSpane,
+  Div,
+  DivBox,
+  DivColorLink,
+  NaVlink,
+} from 'components/Bike/Bike.styled';
 
-const Recomended = ({ title, description, price, url, alt }) => {
+const Recomended = () => {
+  const { model, price, color, brend, img, description } = collection[6];
   return (
-    <Section>
-      <Text>Recomended</Text>
-      <ul>
-        <li>
-          <h2>{title}title</h2>
-        </li>
-        <li>
-          <img src={url} alt={alt} />
-          img
-        </li>
-        <li>
-          <p>{description}description</p>
-        </li>
-        <li>
-          <p>{price}price</p>
-        </li>
-      </ul>
-      <Div>
-        <IconButton>
-          <HeartIcone />
-        </IconButton>
-        <IconButton>
-          <CartIcone />
-        </IconButton>
-      </Div>
-    </Section>
+    <>
+      <Section>
+        <Text>Recomended</Text>
+        <Box>
+          <img src={img} alt={model} style={{ width: '232px' }} />
+          <DivColorLink>
+            <ColorSpane style={{ background: `${color}` }} />
+            <NaVlink to={`${model}`}>
+              {brend} {model}
+            </NaVlink>
+          </DivColorLink>
+          <P style={{ fontSize: '15px' }}>{description}</P>
+          <Div>
+            <P style={{ fontSize: '35px', textTransform: 'uppercase' }}>
+              {price}$
+            </P>
+            <DivBox>
+              <IconButton>
+                <HeartIcone style={{ width: '27px' }}/>
+              </IconButton>
+              <IconButton>
+                <CartIcone />
+              </IconButton>
+            </DivBox>
+          </Div>
+        </Box>
+      </Section>
+    </>
   );
 };
 export default Recomended;
--- src/components/Home/Recomended/Recomended.styled.jsx
+++ src/components/Home/Recomended/Recomended.styled.jsx
@@ -1,12 +1,5 @@
 import styled from '@emotion/styled';
 
-export const Div = styled.div`
-  display: flex;
-  position: absolute;
-  bottom: 60px;
-  right: 25px;
-`;
-
 export const Section = styled.section`
   display: flex;
   flex-direction: column;
@@ -14,3 +7,35 @@ export const Section = styled.section`
   padding: 39px 25px 60px 25px;
   position: relative;
 `;
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const DivBox = styled.div`
+  display: flex;
+`;
+export const P = styled.p`
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+
+line-height: 146.99%;
+/* or 22px */
+
+letter-spacing: 0.04em;
+
+color: #000000;
+margin-top: 25px;
+`;
+// font-family: 'Roboto';
+// font-style: normal;
+// font-weight: 400;
+// font-size: 35px;
+// line-height: 146.99%;
+// /* or 51px */
+
+// letter-spacing: 0.04em;
+// text-transform: uppercase;
+
+// color: #000000;
